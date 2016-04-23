cc2e10e92611d3c3cdae1de9200dee2c278f1166
Ramón José Arellano <ramonmoxo@gmail.com>
2016-04-23 20:31:45 +0200

    "$@"

--- backend/app/csvParser.js
+++ backend/app/csvParser.js
@@ -3,25 +3,44 @@ var parse = require('csv-parse');
 var allAllToDB = function (callback)
 {
 
-  var csv ='"1","2","3","4"\n"a","b","c","d"';
+  var csv ='1900,Drought,CPV,"Cabo Verde",1,11000,,,,,';
   parse(csv, function(err,output){
-    callback(output[0]);
+    if(!err)
+    {
+      output.map(createDBJson);
+      callback("Data added");
+    }
+    else
+      callback("Error adding all csv data");
   });
 
 }
 
-function addItem (statToAdd)
+function addItem (statToAdd, callback)
 {
-  statsDB.add(statToAdd, function (err, res){
-    if (!err)
-    {
-      callback(200);
-    }
-    else
-    {
-      callback(500);
-    }
-  })
+  statsDB.add(statToAdd, function (err,stat) {
+
+  });
+}
+
+function createDBJson (jsonList)
+{
+  var json =
+  {
+    "year" : jsonList[0],
+    "disasterType" : jsonList[1],
+    "iso" : jsonList[2],
+    "countryName" : jsonList[3],
+    "occurrence" : jsonList[4],
+    "totalDeaths" : jsonList[5],
+    "affected" : jsonList[6],
+    "injured" : jsonList[7],
+    "homeless" : jsonList[8],
+    "totalAffected" : jsonList[9],
+    "totalDamage" : jsonList[10]
+  };
+
+  return json;
 }
 
 module.exports = {
